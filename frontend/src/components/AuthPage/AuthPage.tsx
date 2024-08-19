fd1ec24e574e2436a4e1aa4d622ed3bb8a715b65
Roland <machodin@yandex.ru>
2024-08-19 22:23:54 +0300

    added types and validation

--- frontend/src/components/AuthPage/AuthPage.tsx
+++ frontend/src/components/AuthPage/AuthPage.tsx
@@ -1,15 +1,23 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './AuthPage.scss';
 import Input from '../Input/Input';
 import Button from '../Button/Button';
+import { useForm } from 'react-hook-form';
+import { defaultRequiredMessage } from '@/src/lib/constants';
 
 enum AuthState {
     TEL,
     EMAIL,
 }
 
+interface formData {
+    email: string;
+    tel: string;
+}
+
 export default function AuthPage() {
+    const { register, handleSubmit, watch, formState: { errors, isValid }, trigger } = useForm<formData>({ mode: 'all' });
     const [authState, setAuthState] = useState<AuthState>(AuthState.TEL)
 
     function onNextClick() {
@@ -20,6 +28,10 @@ export default function AuthPage() {
 
     }
 
+    useEffect(() => {
+
+    }, [])
+
     return (
         <main className='authPage'>
             <form className='authPage__form'>
@@ -27,17 +39,25 @@ export default function AuthPage() {
                     <>
                         <h2 className={'authPage__title'}>Введите номер телефона</h2>
                         <p className={'authPage__info'}>Доступ к данным будет только у нас.<br /> Будет использован как логин</p>
-                        <Input placeholder='+7 (999) 999-99-99' type='tel' />
-                        <Button onClick={onNextClick} title='Далее' additionalClass='authPage__button' />
-                    </>)}
+                        <Input placeholder='+7 (999) 999-99-99' type='tel' {...register('tel', {
+                            required: { value: true, message: defaultRequiredMessage },
+                            pattern: {
+                                value: /^[+]?\d{1,2}\s?\(?\d{3}\)?\s?\d{3}-|\s?\d{2}-|\s?\d{2}$/,
+                                message: "Некорректный номер телефона",
+                            },
+                        })} />
+                        <Button onClick={onNextClick} title='Далее' additionalClass='authPage__button' disabled={Boolean(errors.tel)} />
+                    </>
+                )}
                 {authState == AuthState.EMAIL && (
                     <>
                         <h2 className={'authPage__title'}>Получите код на ваш email</h2>
                         <p className={'authPage__info'}>Введите адрес электронной почты, мы отправим<br />
                             на нее код для подтверждения регистрации</p>
-                        <Input placeholder='E-mail' type='email' />
-                        <Button onClick={onSendCodeClick} title='Выслать код' additionalClass='authPage__button' />
-                    </>)}
+                        <Input placeholder='E-mail' type='email' {...register('email', { required: { value: true, message: defaultRequiredMessage } })} />
+                        <Button onClick={onSendCodeClick} title='Выслать код' additionalClass='authPage__button' disabled={Boolean(errors.email)} />
+                    </>
+                )}
             </form>
         </main>
     )
