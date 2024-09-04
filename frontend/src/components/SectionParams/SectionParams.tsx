66fef97351cf2b56eed2e542b6324d7e8aeae386
Mihail <mixa.bahrov@mail.ru>
2024-09-04 21:07:23 +0300

    added SectionParams without map

---
+++ frontend/src/components/SectionParams/SectionParams.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import './SectionParams.scss';
+import Input from "../Input/Input";
+
+export default function SectionParams() {
+    return (
+        <div className="section_params">
+            <div className="section_params_container">
+                <h1 className="section_params_title">Олимпийский</h1>
+                <div className="section_params_block">
+                    <div className="section_params_label">
+                        <span>Направление</span>
+                    </div>
+                    <Input additionalClass="dir_input" placeholder="Например, спорт"/>
+                    <div className="section_params_label">
+                        <span>Вид деятельности</span>
+                    </div>
+                    <Input additionalClass="dir_input" placeholder="Например, футбол"/>
+                    <div className="section_params_label">
+                        <span>Вид деятельности</span>
+                    </div>
+                    <Input additionalClass="dir_input" placeholder="Например, футбол"/>
+                    <div className="section_params_label">
+                        <span>Возраст</span>
+                    </div>
+                    <Input additionalClass="dir_input" placeholder="Введите возраст"/>
+                    <div className="section_params_label">
+                        <span>Адрес</span>
+                    </div>
+                    <Input additionalClass="dir_input" placeholder="Введите адрес"/>
+                    <div className="section_params_label">
+                        <span>Уточните место на карте</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
