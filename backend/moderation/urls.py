b8428dfd1d11caf0be42fdc49ae1f32babd409c3
JackCodder <not.a.jack.shenzhen@gmail.com>
2024-09-12 12:06:38 +0500

    Модерация анкеты пользователя.
    Изменения:
    1) В модели User, добавлены новые статусы для блокировки и разблокировки пользователей.
    2) В представление moderation.views.py добавлен метод для блокировки, разблокировки, удаления пользователей и отправки замечаний на почту
    3) Также в этом представлении реализована фильтрация и поиск пользователей.
    4) Реализована цензура текстовых полей у пользователя, пока цензура работает для английских слов без учета изменений в формах слова

--- backend/moderation/urls.py
+++ backend/moderation/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import HelpRequestViewSet
+from .views import HelpRequestViewSet, UserModerationViewSet
 
 app_name = 'moderation'
 
 router = DefaultRouter()
 router.register(r'help-requests', HelpRequestViewSet)
+router.register(r'user-moderation', UserModerationViewSet, basename='user-moderation')
 
 urlpatterns = [
     path('', include(router.urls)),
