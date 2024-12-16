1f96adb6a315de0f81ce446527680443218b7736
ber9z <berez.a@bk.ru>
2024-12-16 18:18:27 +0300

    RoomDatabase методы

--- app/src/main/java/com/example/kidedvisor/core/roomdb/AppDatabase.kt
+++ app/src/main/java/com/example/kidedvisor/core/roomdb/AppDatabase.kt
@@ -2,6 +2,8 @@ package com.example.kidedvisor.core.roomdb
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.kidedvisor.core.roomdb.dao.ActivityBranchDao
+import com.example.kidedvisor.core.roomdb.dao.ActivityTypeDao
 import com.example.kidedvisor.core.roomdb.dao.ClubDao
 import com.example.kidedvisor.core.roomdb.entity.ActivityBranchEntity
 import com.example.kidedvisor.core.roomdb.entity.ActivityTypeEntity
@@ -22,4 +24,6 @@ import com.example.kidedvisor.core.roomdb.entity.ReviewEntity
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun clubDao(): ClubDao
+    abstract fun activityBranch(): ActivityBranchDao
+    abstract fun activityType(): ActivityTypeDao
 }
