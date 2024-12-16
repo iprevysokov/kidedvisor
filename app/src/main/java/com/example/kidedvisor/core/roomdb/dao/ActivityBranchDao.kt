2a5049431189c7b51e96201ace03bb2050409ea2
ber9z <berez.a@bk.ru>
2024-12-16 18:51:52 +0300

    Dao методы

--- app/src/main/java/com/example/kidedvisor/core/roomdb/dao/ActivityBranchDao.kt
+++ app/src/main/java/com/example/kidedvisor/core/roomdb/dao/ActivityBranchDao.kt
@@ -1,9 +1,11 @@
 package com.example.kidedvisor.core.roomdb.dao
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.kidedvisor.core.roomdb.entity.ActivityBranchEntity
 
+@Dao
 interface ActivityBranchDao {
 
     @Insert
