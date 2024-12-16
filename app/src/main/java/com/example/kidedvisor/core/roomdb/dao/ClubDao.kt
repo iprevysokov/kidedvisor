bd8152e644a82ed28e871ea9b6a4d0cf7cbdbe39
ber9z <berez.a@bk.ru>
2024-12-16 12:56:56 +0300

    ClubDao nullable

--- app/src/main/java/com/example/kidedvisor/core/roomdb/dao/ClubDao.kt
+++ app/src/main/java/com/example/kidedvisor/core/roomdb/dao/ClubDao.kt
@@ -7,7 +7,7 @@ import com.example.kidedvisor.core.roomdb.entity.ClubEntity
 @Dao
 interface ClubDao {
     @Query("SELECT *FROM $TABLE_NAME")
-    suspend fun getAllClubs(): ClubEntity
+    suspend fun getAllClubs(): ClubEntity?
 
     companion object {
         const val TABLE_NAME = "Club"
