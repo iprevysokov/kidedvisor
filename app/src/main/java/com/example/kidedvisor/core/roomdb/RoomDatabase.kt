a2555abf32e3b2ac4e301f521450dbe45bc766e9
ber9z <berez.a@bk.ru>
2024-12-16 12:55:24 +0300

    Добавлены ClubDao и RoomDatabase

---
+++ app/src/main/java/com/example/kidedvisor/core/roomdb/RoomDatabase.kt
@@ -0,0 +1,22 @@
+package com.example.kidedvisor.core.roomdb
+
+import androidx.room.Database
+import com.example.kidedvisor.core.roomdb.dao.ClubDao
+import com.example.kidedvisor.core.roomdb.entity.ActivityBranchEntity
+import com.example.kidedvisor.core.roomdb.entity.ActivityTypeEntity
+import com.example.kidedvisor.core.roomdb.entity.ClubEntity
+import com.example.kidedvisor.core.roomdb.entity.CommentEntity
+import com.example.kidedvisor.core.roomdb.entity.ProfileEntity
+import com.example.kidedvisor.core.roomdb.entity.ReviewEntity
+
+@Database(version = 1, entities = [
+    ActivityBranchEntity::class,
+    ActivityTypeEntity::class,
+    ClubEntity::class,
+    ProfileEntity::class,
+    ReviewEntity::class,
+    CommentEntity::class
+])
+abstract class RoomDatabase {
+    abstract fun clubDao(): ClubDao
+}
---
+++ app/src/main/java/com/example/kidedvisor/core/roomdb/dao/ClubDao.kt
@@ -0,0 +1,15 @@
+package com.example.kidedvisor.core.roomdb.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.example.kidedvisor.core.roomdb.entity.ClubEntity
+
+@Dao
+interface ClubDao {
+    @Query("SELECT *FROM $TABLE_NAME")
+    suspend fun getAllClubs(): ClubEntity
+
+    companion object {
+        const val TABLE_NAME = "Club"
+    }
+}
