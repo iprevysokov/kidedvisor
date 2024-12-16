dad489d4e7f30dbe3c054e4ab6a28a13825c7f19
ber9z <berez.a@bk.ru>
2024-12-16 18:15:29 +0300

    dao методы

--- app/src/main/java/com/example/kidedvisor/core/roomdb/dao/ActivityBranchDao.kt
+++ app/src/main/java/com/example/kidedvisor/core/roomdb/dao/ActivityBranchDao.kt
@@ -1,12 +1,16 @@
 package com.example.kidedvisor.core.roomdb.dao
 
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.kidedvisor.core.roomdb.entity.ActivityBranchEntity
 
 interface ActivityBranchDao {
 
     @Insert
     suspend fun addActivityBranch(activityBranchEntity: ActivityBranchEntity)
+
+    @Query("SELECT * FROM $TABLE_NAME")
+    suspend fun getAllActivityBranch(): ActivityBranchEntity?
 
     companion object {
         const val TABLE_NAME = "Activity_Branch"
--- app/src/main/java/com/example/kidedvisor/core/roomdb/dao/ActivityTypeDao.kt
+++ app/src/main/java/com/example/kidedvisor/core/roomdb/dao/ActivityTypeDao.kt
@@ -1,12 +1,16 @@
 package com.example.kidedvisor.core.roomdb.dao
 
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.kidedvisor.core.roomdb.entity.ActivityTypeEntity
 
 interface ActivityTypeDao {
 
     @Insert
     suspend fun addActivityType(activityTypeEntity: ActivityTypeEntity)
+
+    @Query("SELECT * FROM $TABLE_NAME")
+    suspend fun getAllActivityType(): ActivityTypeEntity?
 
     companion object {
         const val TABLE_NAME = "Activity_Type"
