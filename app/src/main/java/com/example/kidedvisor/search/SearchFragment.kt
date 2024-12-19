b984b71b3a5145ca9deb4f6972412f3d582d0dd8
ber9z <berez.a@bk.ru>
2024-12-19 15:29:42 +0300

    Добавление фрагмента, начало верстки

---
+++ app/src/main/java/com/example/kidedvisor/search/SearchFragment.kt
@@ -0,0 +1,34 @@
+package com.example.kidedvisor.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.kidedvisor.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SearchFragment()
+    }
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding: FragmentSearchBinding get() = _binding!!
+
+    private val viewModel: SearchViewModel by viewModels()
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
