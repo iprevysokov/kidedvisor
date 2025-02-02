9ca05417b100bd7c8f0f13d397065803617812c2
nickSwany <Thief.leroy@yandex.ru>
2025-02-02 20:48:57 +0300

    Добавил в эту ветку второй похожий экран

--- app/src/main/java/com/example/kidedvisor/club/presentation/ui/fragment/ConnectionFragment.kt
+++ app/src/main/java/com/example/kidedvisor/club/presentation/ui/fragment/ConnectionFragment.kt
@@ -1,28 +1,29 @@
-package com.example.kidedvisor.club.presentation.ui
+package com.example.kidedvisor.club.presentation.ui.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.kidedvisor.R
-import com.example.kidedvisor.databinding.FragmentRequestCardRightsBinding
+import com.example.kidedvisor.databinding.FragmentConnectionBinding
 
-class RequestCardRightsFragment : Fragment() {
+class ConnectionFragment : Fragment() {
 
-    private var _binding: FragmentRequestCardRightsBinding? = null
+    private var _binding: FragmentConnectionBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentRequestCardRightsBinding.inflate(inflater, container, false)
+        _binding = FragmentConnectionBinding.inflate(inflater, container, false)
         return binding.root
     }
+
 }
