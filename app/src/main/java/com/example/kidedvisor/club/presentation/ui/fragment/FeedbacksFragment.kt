b0a2229a490889fe6055bb4863cf89b2976f04d4
nickSwany <Thief.leroy@yandex.ru>
2025-02-12 20:08:58 +0300

    Сырой вариант

---
+++ app/src/main/java/com/example/kidedvisor/club/presentation/ui/fragment/FeedbacksFragment.kt
@@ -0,0 +1,24 @@
+package com.example.kidedvisor.club.presentation.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kidedvisor.R
+
+class FeedbacksFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_feedbacks, container, false)
+    }
+
+}
