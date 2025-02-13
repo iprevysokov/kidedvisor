fb4ed9229d312775004aa69f143c1c9f1d8a2b38
Vaaargr <Antnsapstray@mail.ru>
2025-02-13 13:01:54 +0300

    Редактирование профиля ui.

---
+++ app/src/main/java/com/example/kidedvisor/profile/presentation/ui/ProfileEditFragment.kt
@@ -0,0 +1,30 @@
+package com.example.kidedvisor.profile.presentation.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.kidedvisor.databinding.FragmentProfileEditBinding
+import com.example.kidedvisor.profile.presentation.viewModel.ProfileEditViewModel
+
+class ProfileEditFragment : Fragment() {
+    private var _binding: FragmentProfileEditBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: ProfileEditViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileEditBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
