68729313850959eab8fec725b071c433c542da21
ber9z <berez.a@bk.ru>
2025-02-25 15:38:19 +0300

    init добавление фрагмента, viewmodel, xml и зависимость

--- app/src/main/java/com/example/kidedvisor/search/di/viewModelModule.kt
+++ app/src/main/java/com/example/kidedvisor/search/di/viewModelModule.kt
@@ -1,11 +1,16 @@
 package com.example.kidedvisor.search.di
 
 import com.example.kidedvisor.search.presenter.SearchViewModel
+import com.example.kidedvisor.search.presenter.filters.FiltersViewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
 
     factory<SearchViewModel> {
         SearchViewModel(get(), get(), get(), get())
+    }
+
+    factory<FiltersViewModel> {
+        FiltersViewModel()
     }
 }
---
+++ app/src/main/java/com/example/kidedvisor/search/ui/filters/FiltersFragment.kt
@@ -0,0 +1,31 @@
+package com.example.kidedvisor.search.ui.filters
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.kidedvisor.databinding.FragmentFiltersBinding
+import com.example.kidedvisor.search.presenter.filters.FiltersViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class FiltersFragment : Fragment() {
+
+    private val viewModel: FiltersViewModel by viewModel()
+
+    private var _binding: FragmentFiltersBinding? = null
+    private val binding: FragmentFiltersBinding = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFiltersBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
