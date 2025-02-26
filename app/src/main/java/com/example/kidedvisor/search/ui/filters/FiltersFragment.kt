b98a2d2d126cea9f7d3a74e24746e8cee2ad4ec5
ber9z <berez.a@bk.ru>
2025-02-26 21:41:09 +0300

    верстка экрана фильтры

--- app/src/main/java/com/example/kidedvisor/search/ui/filters/FiltersFragment.kt
+++ app/src/main/java/com/example/kidedvisor/search/ui/filters/FiltersFragment.kt
@@ -2,9 +2,12 @@ package com.example.kidedvisor.search.ui.filters
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.kidedvisor.R
 import com.example.kidedvisor.databinding.FragmentFiltersBinding
 import com.example.kidedvisor.search.presenter.filters.FiltersViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -14,7 +17,7 @@ class FiltersFragment : Fragment() {
     private val viewModel: FiltersViewModel by viewModel()
 
     private var _binding: FragmentFiltersBinding? = null
-    private val binding: FragmentFiltersBinding = _binding!!
+    private val binding: FragmentFiltersBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +25,10 @@ class FiltersFragment : Fragment() {
     ): View {
         _binding = FragmentFiltersBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onDestroy() {
