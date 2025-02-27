3801aa06f7dd8aba65d463af96437525a11efe9d
ber9z <berez.a@bk.ru>
2025-02-27 19:09:41 +0300

    отображение дефолтных значений фильтра

--- app/src/main/java/com/example/kidedvisor/search/ui/filters/FiltersFragment.kt
+++ app/src/main/java/com/example/kidedvisor/search/ui/filters/FiltersFragment.kt
@@ -2,12 +2,10 @@ package com.example.kidedvisor.search.ui.filters
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
-import com.example.kidedvisor.R
 import com.example.kidedvisor.databinding.FragmentFiltersBinding
 import com.example.kidedvisor.search.presenter.filters.FilterBranchAdapter
 import com.example.kidedvisor.search.presenter.filters.FiltersViewModel
@@ -20,11 +18,20 @@ class FiltersFragment : Fragment() {
     private var _binding: FragmentFiltersBinding? = null
     private val binding: FragmentFiltersBinding get() = _binding!!
 
+    private var minAge = DEF_MIN_AGE
+    private var maxAge = DEF_MAX_AGE
+    private var minRating = DEF_MIN_RATING
+    private var maxRating = DEF_MAX_RATING
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentFiltersBinding.inflate(inflater, container, false)
+
+        setAgeFilter(minAge, maxAge)
+        setRatingFilter(minRating, maxRating)
+
         return binding.root
     }
 
@@ -39,5 +46,30 @@ class FiltersFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    private fun setAgeFilter(minAge: String, maxAge: String) {
+        binding.apply {
+            minAgeEditText.hint = minAge
+            minAgeInputLayout.placeholderText = minAge
+            maxAgeEditText.hint = maxAge
+            maxAgeInputLayout.placeholderText = maxAge
+        }
+    }
+
+    private fun setRatingFilter(minRating: String, maxRating: String) {
+        binding.apply {
+            minRatingEditText.hint = minRating
+            minRatingInputLayout.placeholderText = minRating
+            maxRatingEditText.hint = maxRating
+            maxRatingInputLayout.placeholderText = maxRating
+        }
+    }
+
+    companion object {
+        private const val DEF_MIN_AGE = "4"
+        private const val DEF_MAX_AGE = "16"
+        private const val DEF_MIN_RATING = "0"
+        private const val DEF_MAX_RATING = "5"
     }
 }
