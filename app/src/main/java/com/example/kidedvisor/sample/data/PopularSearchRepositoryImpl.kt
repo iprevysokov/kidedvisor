cd954c12c6a9fa854681c800c184c654131cb39a
Aleksandr Berezovskiy <berez.a@bk.ru>
2025-03-11 16:29:33 +0300

    добавление id для ClubInSearch

--- app/src/main/java/com/example/kidedvisor/sample/data/PopularSearchRepositoryImpl.kt
+++ app/src/main/java/com/example/kidedvisor/sample/data/PopularSearchRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.example.kidedvisor.sample.data
 import com.example.kidedvisor.R
 import com.example.kidedvisor.sample.domain.api.PopularSearchRepository
 import com.example.kidedvisor.search.domain.models.ClubInSearch
+import java.util.UUID
 
 class PopularSearchRepositoryImpl : PopularSearchRepository {
     override fun getPopularRequest(): List<String> {
@@ -18,6 +19,7 @@ class PopularSearchRepositoryImpl : PopularSearchRepository {
     override fun getPopularClubs(): List<ClubInSearch> {
         return listOf(
             ClubInSearch(
+                id = UUID.fromString("8C0A9050-F44A-4766-8E48-FAEA54369708"),
                 rating = 4.2,
                 name = "Школа искусств \"Тутти\"",
                 address = "Металлистов, д.1",
@@ -26,6 +28,7 @@ class PopularSearchRepositoryImpl : PopularSearchRepository {
                 type = "Танцы",
             ),
             ClubInSearch(
+                id = UUID.fromString("E18BA298-B80D-46BE-9D72-CEA20176DA59"),
                 rating = 4.2,
                 name = "Художественная студия \"Пикассо\"",
                 address = "Металлистов, д.1",
@@ -34,6 +37,7 @@ class PopularSearchRepositoryImpl : PopularSearchRepository {
                 type = "Рисование",
             ),
             ClubInSearch(
+                id = UUID.fromString("73245E2F-0152-427A-AD1A-1B034595EC52"),
                 rating = 4.2,
                 name = "Художественная студия \"Пикассо\"",
                 address = "Металлистов, д.1",
@@ -42,6 +46,7 @@ class PopularSearchRepositoryImpl : PopularSearchRepository {
                 type = "Рисование",
             ),
             ClubInSearch(
+                id = UUID.fromString("D29D68E2-27A6-4888-9247-784E68BF1E13"),
                 rating = 4.2,
                 name = "Художественная студия \"Пикассо\"",
                 address = "Металлистов, д.1",
@@ -50,6 +55,7 @@ class PopularSearchRepositoryImpl : PopularSearchRepository {
                 type = "Рисование",
             ),
             ClubInSearch(
+                id = UUID.fromString("8A7565BD-F1C6-4830-90FD-9C3F0360CE4E"),
                 rating = 4.2,
                 name = "Мир единоборств чертаново \"Пикассо\"",
                 address = "Металлистов, д.1",
--- app/src/main/java/com/example/kidedvisor/search/domain/models/ClubInSearch.kt
+++ app/src/main/java/com/example/kidedvisor/search/domain/models/ClubInSearch.kt
@@ -2,8 +2,10 @@ package com.example.kidedvisor.search.domain.models
 
 import androidx.annotation.DrawableRes
 import com.example.kidedvisor.R
+import java.util.UUID
 
 data class ClubInSearch(
+    val id: UUID,
     val name: String,
     val address: String,
     val branch: String,
